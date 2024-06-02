79d756046321a6dad4b029d6d9b21521c29ad645
Eugene <eugene_chernyshev@yahoo.com>
2024-06-02 11:21:36 +0300

    Lander home
    The project empty title page will be shown

--- lander/views.py
+++ lander/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {
+        'title': 'lander'
+    }
+
+    return render(request, 'lander/index.html', context=context)
